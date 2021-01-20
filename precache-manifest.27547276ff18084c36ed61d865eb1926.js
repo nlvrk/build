96c1e9c58f1c703c7c82c697b4f321c1acce4d3c
Nlvrk <exaile55@mail.ru>
2021-01-20 12:55:26 +0600

    Initial commit

---
+++ precache-manifest.27547276ff18084c36ed61d865eb1926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de2002f33c4dd43fb3c791881bdd091c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "466d7a35a84eed84ecd6",
+    "url": "/static/css/main.2e063783.chunk.css"
+  },
+  {
+    "revision": "11a802cc0c439b26253d",
+    "url": "/static/js/2.6e630f4c.chunk.js"
+  },
+  {
+    "revision": "379c198c7296ef64963a14f3d7af9e55",
+    "url": "/static/js/2.6e630f4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466d7a35a84eed84ecd6",
+    "url": "/static/js/main.29a4f393.chunk.js"
+  },
+  {
+    "revision": "323005383fb984511008",
+    "url": "/static/js/runtime-main.97f79fc8.js"
+  }
+]);
